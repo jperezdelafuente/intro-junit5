d14a0eff896f23b14fb386f0170503e0bfdebd58
jperezdelafuente <jesusperez82@gmail.com>
2022-05-23 18:43:06 +0200

    adding test with @BeforeEach

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,27 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("I´m before each...");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println(greeting.helloWorld("John"));
+    }
+}
